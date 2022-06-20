ac47e7affd4cc99631d9347d6cfd2572b4ffef4a
Agustin_Lopez <agustin2000lopez@gmail.com>
2022-06-19 22:36:21 -0300

    Se agrega README.md a ejemplo1

---
+++ ejemplo1/README.md
@@ -0,0 +1,3 @@
+En este directorio "ejemplo1" puse a práctica al sistema de módulos de Node.js para importar una funcion en mi "app.js" desde el otro archivo "carrito.js utilizando require("ruta del archivo") que en este caso era 'const carrito = require("./carrito")'. y desde el archivo "carrito.js" exporté el array utilizando module.exports.
+
+
--- ejemplo1/app.js
+++ ejemplo1/app.js
@@ -1,8 +1,12 @@
-//Utilizo el require para importar una función desde otro archivo a mi app.js 
+/* Nodejs tiene un sistema de modulos incorporado. Un archivo Node.js puede
+ importar una funcionalidad expuesta por otros archivos. Con require y module.exports.
+
+Utilizo el require para importar una función desde otro archivo a mi app.js */
 
 const carrito = require("./carrito")
 
 carrito.forEach((fruta) => {
     console.log(fruta);
 });
+
 
